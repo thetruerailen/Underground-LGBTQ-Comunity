f3be02d541f8f26b29a118497d350caabfc66718
thetruerailen <103075692+thetruerailen@users.noreply.github.com>
2022-04-22 08:10:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 welcomemessage = ("Welcome to the Underground LGBTQ+ Comunity! You can login to our stuff on the links bellow!")
 link1 = ("https://lgbtq.railenbailenservers.com")
 link2 = ("https://LGBTQ-Hangout-Chat.railenbailen.repl.co")
@@ -7,3 +9,4 @@ print(link1)
 print(link2)
 print(link3)
 print("Warning, the Discord server link is replaced every 50 members that join so know that when it is not working, just email us at demoss.railen@railenbailen.co and the admins will help you!")
+wait(100)
